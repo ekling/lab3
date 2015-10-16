f4f6ecf8247bcc959dc5825c342931fcca6103f5
Emil Kling <emil.kling1@gmail.com>
2015-10-16 21:52:00 +0200

    fixes

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ from config import app
 @app.route('/', methods=['GET'])
 def count_tweets():
 
-    queue = [parse.s('tweets_{}.txt'.format(x)) for x in xrange(14,16)]
+    queue = [parse.s('tweets_{}.txt'.format(x)) for x in xrange(0,20)]
     g = group(queue)
 
     res = g()
